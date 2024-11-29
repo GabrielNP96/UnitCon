0bd31da73cd86d296a9b42b0c27241a521d9ce59
GabrielNP96 <gabrielnp1996@gmail.com>
2024-11-29 00:07:17 -0300

    validação pronta.

---
+++ modules/temperature/temperatureConverter.js
@@ -0,0 +1,18 @@
+function temperatureConverter(num, from, to) {
+    if(isNaN(num) === false && typeof(from) === "string" && typeof(to) === "string" && from.length === 1 && to.length === 1) {
+        const originalTemperature = from.toLowerCase();
+        const newTemperature = to.toLowerCase();
+        
+        if(from === 'c') {
+            
+        }
+
+    } else {
+        console.log('not');
+    }
+
+    
+
+}
+
+temperatureConverter(1,'a', 'b');

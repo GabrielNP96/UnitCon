5b93d73181a6e3dc2a72a5d485b0cf7f84639585
GabrielNP96 <gabrielnp1996@gmail.com>
2024-11-28 23:52:22 -0300

    recomeçando aplicação

--- modules/temperature/toCelsius.js
+++
@@ -1,9 +0,0 @@
-export default function convertToCelsius(num, temperature) {
-    temperature = temperature;
-    switch(temperature) {
-        case 'f':
-            return (num - 32) * 5/9;
-        case 'k':
-            return num - 273,15;
-    }
-}
